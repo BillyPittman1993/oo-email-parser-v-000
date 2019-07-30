0e1befabf604e325f8f54fcc08eea9535323b892
BillyPittman1993 <billy.pittman@flatironschool.com>
2019-07-30 18:41:53 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -14,7 +14,8 @@ class EmailParser
   
   def parse
   email_array = @emails.split(/[, ]/).uniq
-  email_array.reject! {element| element
+  email_array.reject! {element| element.empty?}
+  email_array
   end
     
 end
